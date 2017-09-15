28b58bcac43067e5fa188b6a9d8af0004c9fe242
Wang <gavin19950511@gmail.com>
2017-09-15 21:06:58 +0800

    Update WebAction.py

--- WebAction.py
+++ WebAction.py
@@ -15,6 +15,7 @@ elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source,"No results found."
+driver.close()
 '''
 
 #    Open Web browser
